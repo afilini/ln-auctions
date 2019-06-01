92b858eeb1c08d6facf707b128ebd9f503ee89cc
Alekos Filini <alekos.filini@gmail.com>
2019-06-01 14:03:23 +0200

    Code wrote live on stage

--- index.js
+++ index.js
@@ -12,5 +12,30 @@ const startDate = new Date();
 const server = new JSONRpcServer();
 const engine = new AuctionsEngine(startDate);
 
-const rpc = new LightningClient('/home/user/.lightning/');
-webserver(rpc, engine);
+server.on('getmanifest', (msg) => {
+    msg.reply({
+        options: [],
+        rpcmethods: [],
+        subscriptions: [],
+        hooks: ["invoice_payment"]
+    });
+});
+
+server.on('init', (msg) => {
+    const client = new LightningClient(msg.configuration['lightning-dir']);
+    webserver(client, engine);
+});
+
+server.on('invoice_payment', (payment) => {
+    if (engine.getState() != 'RUNNING') {
+        payment.reply({failure_code: 21});
+        return;
+    }
+
+    const amount = parseInt(payment.payment.msat.replace('msat', ''));
+    debug(amount);
+
+    engine.newOffer(amount, payment.payment.label)
+        .then(() => payment.reply({}))
+        .catch(() => payment.reply({failure_code: 21}));
+});
